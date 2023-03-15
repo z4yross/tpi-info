0ef7742cd053508d25d3a242d98be22fd577b86c
z4yross <z4yross@gmail.com>
2023-03-15 15:39:25 -0500

    base

--- src/components/empatymap/main.jsx
+++ src/components/empatymap/main.jsx
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
 
+import Section from "./section/section.jsx";
+
 export default function EmpathyMap(){
+
+    const sections = [
+        "hears",
+        "thinks",
+        "hurts",
+        "says",
+        "wonders",
+    ];
+
     return (
         <div className="empathymap-container">
-            EMPATHY MAP
+            {sections.map((section, index) => {
+                return (
+                    <Section
+                        key={index}
+                        section={section}
+                    />
+                );
+            })}
         </div>
     );
 }
