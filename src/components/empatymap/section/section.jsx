6d2ea3e8309d481e72726d092e11a85a81b03f43
z4yross <z4yross@gmail.com>
2023-03-20 21:31:08 -0500

    emp cards

--- src/components/empatymap/section/section.jsx
+++ src/components/empatymap/section/section.jsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from "react";
 export default function Section(props) {
 	const [active, setActive] = useState(false);
 
-    const capitalize = (s) => {
-        if (typeof s !== 'string') return ''
-        return s.charAt(0).toUpperCase() + s.slice(1)
-    }
+	const capitalize = (s) => {
+		if (typeof s !== "string") return "";
+		return s.charAt(0).toUpperCase() + s.slice(1);
+	};
 
 	return (
 		// conditional className
@@ -17,11 +17,36 @@ export default function Section(props) {
 			{!active ? (
 				<div className="deactivated-card">
 					<div className="title-card">
-                        {capitalize(props.section)}
-                    </div>					
+						{capitalize(props.section)}
+					</div>
 				</div>
 			) : (
-				props.section
+				<div className="activated-card">
+					<div className="title-card">
+						{capitalize(props.section)}
+					</div>
+					<div className="content-card">
+						<div className="separator"></div>
+						<div className="items">
+							<ul>
+								<li>Item 1</li>
+								<li>Item 2</li>
+								<li>Item 3</li>
+								<li>Item 4</li>
+								<li>Item 5</li>
+							</ul>
+						</div>
+						<div className="separator"></div>
+						<div clasName="quote">
+							<blockquote>
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Quisquam, quod.
+								</p>
+							</blockquote>
+						</div>
+					</div>
+				</div>
 			)}
 		</div>
 	);
